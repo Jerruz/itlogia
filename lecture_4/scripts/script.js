4c452ab72e578c3c73915c63fc90874999639c98
Jerruz <jerruz@jerruz.com>
2024-08-18 17:30:14 +0300

    added event on clic menu

--- lecture_4/scripts/script.js
+++ lecture_4/scripts/script.js
@@ -2,6 +2,17 @@
 // title.style.color = "red";
 // console.log(title)
 
+// навигация по кнопке "купить"
 document.getElementById("main-action-button").onclick = function () {
-    document.getElementById("products").scrollIntoView({behavior: "smooth"});
+    let scroll = document.getElementById("products");
+    scroll.scrollIntoView({behavior: 'smooth'});
+}
+
+// навигация по меню
+const links = document.querySelectorAll(".menu-item > a");
+
+for (let i = 0; i < links.length; i++) {
+    links[i].onclick = function () {
+        document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: 'smooth'});
+    }
 }
