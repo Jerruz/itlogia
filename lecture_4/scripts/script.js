7cac8e92c337cea80712eb1ef6ec502e0dba0b1a
Jerruz <jerruz@jerruz.com>
2024-08-18 19:21:43 +0300

    lecture_4 finished

--- lecture_4/scripts/script.js
+++ lecture_4/scripts/script.js
@@ -22,13 +22,12 @@ const buttons = document.querySelectorAll(".products-item button");
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function () {
-        document.getElementById("order").scrollIntoView({behavior: 'smooth'})   ;
+        document.getElementById("order").scrollIntoView({behavior: 'smooth'});
     }
 }
 
 // переключаем валюту
 const prises = document.getElementsByClassName("products-item-price");
-console.log(prises);
 document.getElementById("change-currency").onclick = function (e) {
     const currentCurrency = e.target.innerText;
 
@@ -45,5 +44,30 @@ document.getElementById("change-currency").onclick = function (e) {
 
     for (let i = 0; i < prises.length; i++) {
         prises[i].innerText = +(prises[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
+    }
+}
+
+// валидация формы
+const product = document.getElementById("product");
+const name = document.getElementById("name");
+const phone = document.getElementById("phone");
+
+document.getElementById("order-action").onclick = function () {
+    let hasError = false;
+
+    [product, name, phone].forEach(item => {
+        if (!item.value) {
+            item.style.borderColor = "red";
+            hasError = true;
+        } else {
+            item.style.borderColor = "";
+        }
+    });
+
+    if (!hasError) {
+        [product, name, phone].forEach(item => {
+            item.value = "";
+        })
+        alert("Спасибо за заказ! Мы скоро свяжемся с Вами!")
     }
 }
