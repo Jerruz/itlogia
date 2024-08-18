30a4d89d7a3ddb13fcb6a8f6e24a8e7198f512c6
Jerruz <jerruz@jerruz.com>
2024-08-18 19:00:20 +0300

    added converter

--- lecture_4/scripts/script.js
+++ lecture_4/scripts/script.js
@@ -2,7 +2,7 @@
 // title.style.color = "red";
 // console.log(title)
 
-// навигация по кнопке "купить"
+// навигация по кнопке "закзать"
 document.getElementById("main-action-button").onclick = function () {
     let scroll = document.getElementById("products");
     scroll.scrollIntoView({behavior: 'smooth'});
@@ -14,5 +14,36 @@ const links = document.querySelectorAll(".menu-item > a");
 for (let i = 0; i < links.length; i++) {
     links[i].onclick = function () {
         document.getElementById(links[i].getAttribute("data-link")).scrollIntoView({behavior: 'smooth'});
+    }
+}
+
+// события на кнопки заказать
+const buttons = document.querySelectorAll(".products-item button");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("order").scrollIntoView({behavior: 'smooth'})   ;
+    }
+}
+
+// переключаем валюту
+const prises = document.getElementsByClassName("products-item-price");
+console.log(prises);
+document.getElementById("change-currency").onclick = function (e) {
+    const currentCurrency = e.target.innerText;
+
+    let newCurrency = "$";
+    let coefficient = 1;
+    if (currentCurrency === "$") {
+        newCurrency = "₽";
+        coefficient = 90;
+    } else if (currentCurrency === "₽") {
+        newCurrency = "BYN";
+        coefficient = 3;
+    }
+    e.target.innerText = newCurrency;
+
+    for (let i = 0; i < prises.length; i++) {
+        prises[i].innerText = +(prises[i].getAttribute("data-base-price") * coefficient).toFixed(1) + " " + newCurrency;
     }
 }
